df24fd9954827eb0ec04c479cebb6bb9ea1bf8ff
Yuri Drobyshevsky <ydrobyshevsky@gmail.com>
2016-05-15 19:00:22 +0300

    button

--- main.c
+++ main.c
@@ -3,18 +3,27 @@
  
 int main(void)
 {
-    const int msDelay = 1000;
+//    const int msDelay = 1000;
  
     // PortB pin4 to output (set bit to 1 using SHL)
-    DDRB = 1<<DDB4;
+    //DDRB = 1<<DDB4;
+    DDRB  |= (1<<PB4);
  
     // PortB to low
-    PORTB = 0;
+    PORTB |= (1<<PB3);
  
     while (1) {
         // XOR on pin 4
-        PORTB ^= 1<<PB4;
-        _delay_ms(msDelay);
+	if (PINB & (1<<PB3)){
+        	//PORTB ^= 1<<PB4;
+                PORTB |= (1<<PB4);
+               // _delay_ms(200);
+       	}
+        else {
+           PORTB &= ~(1<<PB4);
+         }
+	//_delay_ms(msDelay);
+
     }
  
     return 0;
